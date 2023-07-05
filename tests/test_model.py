2e60519a86d1cb17cfe5337f00b75fa0cfd664c9
Ying-Qiu Zheng <yingqiuz12fdu@gmail.com>
2023-07-05 13:57:39 +0100

    add tests_model

--- tests/test_model.py
+++ tests/test_model.py
@@ -4,9 +4,10 @@ from glob import glob
 from pathlib import Path
 path_to_data = Path(__file__).parent / 'test_data'
 
-from localise.load import load_features
+from localise.load import load_features, load_data
 from localise.flatten_batch import FlattenedCRFBatchTensor
-from localise.flatten_forward import FlexibleClassifier
+from localise.flatten_forward import FlexibleClassifier, MLP
+from localise.train import train_loop
 import torch
 
 
@@ -26,3 +27,24 @@ def test_flexibleclassifier():
     model = FlexibleClassifier(torch.nn.Linear(150, 2), is_crf=False, n_classes=2)
     output = model(batch)
     assert list(output.shape) == [batch.X.shape[0], 2]
+
+
+def test_MLP():
+    # Define input_dim, hidden_dim, output_dim, and create a model instance
+    input_dim = 10
+    hidden_dim = 5
+    output_dim = 2
+    model = MLP(input_dim, hidden_dim, output_dim)
+
+    # Create a mock input tensor with size (batch_size, input_dim)
+    batch_size = 32
+    mock_input = torch.randn(batch_size, input_dim)
+
+    # Perform a forward pass
+    output = model(mock_input)
+
+    # Check that the output has the expected size
+    assert output.size() == (batch_size, output_dim)
+
+    # Check that the output is not NaN or infinite
+    assert torch.all(torch.isfinite(output))
