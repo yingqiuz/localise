3b594ddf82af8bb1a0a0e73cafd7b5896e1d3f42
Ying-Qiu Zheng <yingqiuz12fdu@gmail.com>
2023-07-05 16:02:56 +0100

    test_load

--- localise/train.py
+++ localise/train.py
@@ -1,69 +1,97 @@
 import torch
+import random
 from torch import nn
 from torch import optim
-import time
-from functools import partial
 from torch.nn import Linear
 from localise.flatten_forward import FlexibleClassifier, MLP
+import logging
 
 
 def train_loop(data, model, loss_fn, optimizer, lambda_l1, lambda_l2, print_freq=10):
-    size = len(data)
+    """
+    Function to execute one training loop epoch.
 
-    # Set the model to training mode
+    Parameters:
+    data (iterables): Iterable of tuples 
+        containing the training data, (FlattenedCRFBatchTensor, torch.tensor)
+    model (nn.Module): PyTorch model to train
+    loss_fn (nn.modules.loss): Loss function to optimize
+    optimizer (optim.Optimizer): Optimizer to use for training
+    lambda_l1 (float): L1 regularization weight
+    lambda_l2 (float): L2 regularization weight
+    print_freq (int, optional): Frequency to print training loss. Default: 10
+    """
+
+    size = len(data)
     model.train()
+
     for batch, (X, y) in enumerate(data):
-        # Compute prediction and loss
         pred = model(X)
         loss = loss_fn(pred, y)
 
-        # Compute penalty parameters before the loop
+        # add L1 and L2 penalty
         layer_weights = torch.stack([x for x in model.layer.parameters() if x.dim() > 1])
-
-        # Add L1 and L2 regularization
-        l1_penalty = lambda_l1 * torch.norm(layer_weights, 1)
-        l2_penalty = lambda_l2 * torch.norm(layer_weights, 2)
-        loss += l1_penalty + l2_penalty
-
-        # Backpropagation
+        loss += lambda_l1 * torch.norm(layer_weights, 1) + lambda_l2 * torch.norm(layer_weights, 2)
         loss.backward()
         optimizer.step()
         optimizer.zero_grad()
-
         if batch % print_freq == 0:
-            loss, current = loss.item(), (batch + 1)
-            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+            logging.info(f"loss: {loss.item():>7f}  [{batch + 1:>5d}/{size:>5d}]")
 
 
-def test_loop(data, model, loss_fn):
-    # Set the model to evaluation mode - important for batch normalization and dropout layers
-    # Unnecessary in this situation but added for best practices
+def val_loop(data, model, loss_fn):
+    """
+    Function to execute one validation loop epoch.
+
+    Parameters:
+    data (iterables): Iterable of tuples 
+        containing the validation data, (FlattenedCRFBatchTensor, torch.tensor)
+    model (nn.Module): PyTorch model to evaluate
+    loss_fn (nn.modules.loss): Loss function to compute loss
+    """
+
     model.eval()
-    #size = len(data)
-    num_batches = len(data)
     test_loss = 0
-
-    # Evaluating the model with torch.no_grad() ensures that no gradients are computed during test mode
-    # also serves to reduce unnecessary gradient computations and memory usage for tensors with requires_grad=True
     with torch.no_grad():
-        for _, (X, y) in enumerate(data):
+        for X, y in data:
             pred = model(X)
             test_loss += loss_fn(pred, y).item()
-            #correct += (pred.argmax(1) == y).type(torch.float).sum().item()
-
-    test_loss /= num_batches
-    #correct /= size
-    print(f"Test Error: \n Avg loss: {test_loss:>8f} \n")
+    logging.info(f"Test Error: \n Avg loss: {test_loss / len(data):>8f} \n")
 
 
 def train(training_data, test_data, 
-          model_constructor="Linear",
+          model="Linear",
           loss_fn=nn.CrossEntropyLoss(), 
-          optimizer_constructor=optim.Adam, 
+          optimizer=optim.Adam, 
           n_epochs=100, 
           spatial_model=True, 
           l1_lambda=1e-3, l2_lambda=1e-3, lr=1e-3,
           print_freq=10, model_save_path=None):
+    """
+    Function to train a PyTorch model and evaluate it using provided training and test data.
+
+    Parameters:
+    training_data (Iterables): Iterable of tuples 
+        containing the training data, (FlattenedCRFBatchTensor, torch.tensor)
+    test_data (Iterables): Iterable of tuples 
+        containing the test data, (FlattenedCRFBatchTensor, torch.tensor)
+    model (str, optional): String representing the type of model to use. 
+        Options: "Linear", "MLP", or PyTorch model class. Default: "Linear"
+    loss_fn (torch.nn.modules.loss, optional): 
+        Loss function. Default: CrossEntropyLoss
+    optimizer (torch.optim.Optimizer, optional): 
+        Optimizer to use for training. Default: Adam
+    n_epochs (int, optional): Number of training epochs. Default: 100
+    spatial_model (bool, optional): If True, use spatial model. Default: True
+    l1_lambda (float, optional): Weight for L1 regularization. Default: 1e-3
+    l2_lambda (float, optional): Weight for L2 regularization. Default: 1e-3
+    lr (float, optional): Learning rate for the optimizer. Default: 1e-3
+    print_freq (int, optional): Frequency of loss print statements. Default: 10
+    model_save_path (str, optional): Path where the trained model will be saved. If None, the model will not be saved. Default: None
+
+    Returns:
+    m (torch.nn.Module): The trained PyTorch model
+    """
 
     # get dimensions
     X, y = training_data[0]
@@ -72,27 +100,27 @@ def train(training_data, test_data,
     n_kernels = X.f.shape[0]
 
     # Define a model
-    if model_constructor == "Linear":
-        model = FlexibleClassifier(Linear(n_features, n_classes), n_classes=n_classes, 
+    if model == "Linear":
+        m = FlexibleClassifier(Linear(n_features, n_classes), n_classes=n_classes, 
                                           n_kernels=n_kernels, is_crf=spatial_model)
-    elif model_constructor == "MLP":
-        model = FlexibleClassifier(MLP(n_features, 2, n_classes), n_classes=n_classes, 
+    elif model == "MLP":
+        m = FlexibleClassifier(MLP(n_features, 2, n_classes), n_classes=n_classes, 
                                        n_kernels=n_kernels, is_crf=spatial_model)
     else:
-        model = FlexibleClassifier(model_constructor, n_classes=n_classes, 
+        m = FlexibleClassifier(model, n_classes=n_classes, 
                                    n_kernels=n_kernels, is_crf=spatial_model)
 
-    optimizer = optimizer_constructor(model.parameters(), lr=lr)
+    optimizer = optimizer(m.parameters(), lr=lr)
     for t in range(n_epochs):
-        print(f"Epoch {t+1}\n-------------------------------")
-        train_loop(training_data, model, loss_fn, optimizer, l1_lambda, l2_lambda, print_freq)
-        test_loop(test_data, model, loss_fn)
+        logging.info(f"Epoch {t+1}\n-------------------------------")
+        train_loop(training_data, m, loss_fn, optimizer, l1_lambda, l2_lambda, print_freq)
+        val_loop(test_data, m, loss_fn)
 
-    print("Done!")
+    logging.info("Training Done!")
     
     # save the model if requested 
     if model_save_path is not None:
-        torch.save(model.state_dict(), model_save_path)
-        print(f"Model saved to {model_save_path}")
+        torch.save(m.state_dict(), model_save_path)
+        logging.info(f"Model saved to {model_save_path}")
 
-    return model
+    return m
