b71a0f2fab2baa40a8d2640082ed10f52297399f
Ying-Qiu Zheng <yingqiuz12fdu@gmail.com>
2023-07-05 12:55:59 +0100

    add more test funcs for load

--- tests/test_data.py
+++ tests/test_data.py
@@ -20,6 +20,13 @@ def test_load_features():
     assert batch.f.shape[0] == 1
     assert batch.f.dim() == 3
     
+    batch = load_features(subject=f'{path_to_data}/100610', 
+              mask_name='roi/left/tha_small.nii.gz', 
+              target_path='streamlines/left', 
+              atlas='roi/left/atlas.nii.gz', power=[0.5, 1, 2],
+              target_list=['seeds_to_11101_1.nii.gz', 'seeds_to_11102_1.nii.gz'])
+    assert batch.X.shape[1] == 7
+    
     subject='100610'
     target_list = [os.path.split(f)[-1] for f in sorted(glob(f'{path_to_data}/'+subject+'/streamlines/left/seeds_to_*'))]
 
@@ -28,7 +35,17 @@ def test_load_features():
     batch = load_features(subject=os.path.join(f'{path_to_data}', subject), 
               mask_name='roi/left/tha_small.nii.gz', 
               target_path='streamlines/left', 
-              target_list=target_list, power=power, gamma=gamma)
+              target_list=target_list, power=power, gamma=gamma, 
+              output_fname='streamlines/left/features75.npy')
+
+    assert batch.X.shape[1] == len(target_list) * len(power)
+    assert batch.f.shape[0] == len(gamma)
+    assert batch.f.shape[1] == batch.X.shape[0]
+    
+    batch = load_features(subject=os.path.join(f'{path_to_data}', subject), 
+              mask_name='roi/left/tha_small.nii.gz', 
+              power=power, gamma=gamma, 
+              data='streamlines/left/features75.npy')
 
     assert batch.X.shape[1] == len(target_list) * len(power)
     assert batch.f.shape[0] == len(gamma)
@@ -48,10 +65,11 @@ def test_load_data():
                       mask_name=mask_name, 
                       label_name=label_name,
                       power=[1, 2],
+                      atlas='roi/left/atlas.nii.gz',
                       target_path='streamlines/left', 
                       target_list=['seeds_to_11101_1.nii.gz', 'seeds_to_11102_1.nii.gz'])
     assert batch[0].X.shape[0] == batch[1].shape[0]
-    assert batch[0].X.shape[1] == 4
+    assert batch[0].X.shape[1] == 5
     assert isinstance(batch, tuple)
     assert isinstance(batch[0], FlattenedCRFBatchTensor)
     assert len(batch) == 2
