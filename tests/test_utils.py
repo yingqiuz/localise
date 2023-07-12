952387524a7ceafd55c90694f7f3a0a97711e46d
Ying-Qiu Zheng <yingqiuz12fdu@gmail.com>
2023-07-12 13:51:58 +0100

    fix test_utils

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,4 +1,5 @@
 import os
+import numpy as np
 from localise.utils import save_nifti
 from pathlib import Path
 import nibabel as nib
@@ -8,12 +9,12 @@ path_to_data = Path(__file__).parent / 'test_data'
 
 def test_save_nifti():
     subject = '100610'
-    mask = os.path.join(subject, 'roi', 'left', 'tha.nii.gz')
+    mask = os.path.join(path_to_data, subject, 'roi', 'left', 'tha.nii.gz')
     mask_data = nib.load(mask).get_fdata()
-    output_fname = os.path.join(subject, 'saved_file.nii.gz')
+    output_fname = os.path.join(path_to_data, subject, 'saved_file.nii.gz')
 
     vectors = np.random.randn(np.count_nonzero(mask_data))
     save_nifti(vectors, mask, output_fname)
     
     saved_data = nib.load(output_fname).get_fdata()
-    assert np.all(saved_data[mask_data != 0] == vectors)
+    assert np.allclose(saved_data[mask_data != 0], vectors, atol=1e-6, rtol=1e-5)
