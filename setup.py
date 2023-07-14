bc87accc31cab8d4fe36a0362e3caff0cf9dec47
Ying-Qiu Zheng <yingqiuz12fdu@gmail.com>
2023-07-14 14:32:11 +0100

    rewrite setup file

--- setup.py
+++ setup.py
@@ -2,19 +2,23 @@
 
 from setuptools import setup
 
-with open('requirements.txt','rt') as f:
+with open('requirements.txt', 'rt') as f:
     install_requires = [line.strip() for line in f.readlines()]
 
+with open('README.md', 'rt') as f:
+    long_description = f.read()
+
 setup(
-    name='python-localise',
+    name='localise',
     version='0.1.0',  # Update the version number for new releases
     url='https://git.fmrib.ox.ac.uk/yqzheng1/python-localise',
     license='MIT',
     author='Ying-Qiu Zheng',
     author_email='ying-qiu.zheng@ndcn.ox.ac.uk',
     description='A Python package for localisation',
-    long_description=open('README.md').read(),
+    long_description=long_description,
     long_description_content_type='text/markdown',
     packages=['localise',],
     install_requires=install_requires,
+    python_requires='>=3.7',
 )
