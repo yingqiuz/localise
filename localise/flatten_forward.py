ea69eb3ebcbe60cf5b130ae9f4fa28d40b18956a
Ying-Qiu Zheng <yingqiuz12fdu@gmail.com>
2023-07-15 21:39:42 +0100

    add docstrings

--- localise/flatten_forward.py
+++ localise/flatten_forward.py
@@ -4,6 +4,16 @@ from torch.nn.functional import relu, softmax
 
 
 class FlexibleClassifier(Module):
+    """
+    A flexible classifier model, which can be optionally run with Conditional Random Field (CRF) logic.
+    
+    Args:
+        layer (nn.Module): The layer to be used for classification.
+        n_classes (int, optional): The number of output classes. Defaults to 2.
+        n_kernels (int, optional): The number of kernels to use. Defaults to 1.
+        is_crf (bool, optional): If True, uses CRF for forward computation. Defaults to False.
+        n_iter (int, optional): The number of iterations for CRF computation. Defaults to 3.
+    """
     def __init__(self, layer, n_classes=2, n_kernels=1, is_crf=False, n_iter=3):
         super().__init__()
         self.layer = layer
@@ -23,13 +33,40 @@ class FlexibleClassifier(Module):
         #else:
             #return super().parameters()
     def forward(self, data):
+        """
+        Forward propagation for the classifier.
+        
+        Args:
+            data: The input data.
+        
+        Returns:
+            The output from the forward propagation.
+        """
         return self.forward_crf(data) if self.is_crf else self.forward_no_crf(data)
 
     def forward_no_crf(self, data):
+        """
+        Forward propagation without using CRF.
+        
+        Args:
+            data: The input data.
+        
+        Returns:
+            The output from the forward propagation.
+        """
         y = self.layer(data.X)
         return softmax(y, dim=1)
 
     def forward_crf(self, data):
+        """
+        Forward propagation using CRF.
+        
+        Args:
+            data: The input data.
+        
+        Returns:
+            The output from the forward propagation.
+        """
         y = self.layer(data.X) # negative unary potential
         h = y.clone()
         for _ in range(self.n_iter):
@@ -40,6 +77,14 @@ class FlexibleClassifier(Module):
 
 
 class MLP(Module):
+    """
+    A simple Multi-layer Perceptron (MLP) model.
+    
+    Args:
+        input_dim (int): The dimension of the input.
+        hidden_dim (int): The dimension of the hidden layer.
+        output_dim (int): The dimension of the output layer.
+    """
     def __init__(self, input_dim, hidden_dim, output_dim):
         super(MLP, self).__init__()
         self.layer1 = Linear(input_dim, hidden_dim)
@@ -47,6 +92,15 @@ class MLP(Module):
         self.relu = ReLU()
 
     def forward(self, x):
+        """
+        Forward propagation for the MLP.
+        
+        Args:
+            x: The input data.
+        
+        Returns:
+            The output from the forward propagation.
+        """
         x = self.layer1(x)
         x = self.relu(x)
         x = self.layer2(x)
