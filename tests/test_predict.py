ad1de795d3a4a34c0c680d5719fd06e9b40c5c8c
Ying-Qiu Zheng <yingqiuz@users.noreply.github.com>
2023-08-22 18:30:28 +0100

    test spatial is false

--- tests/test_predict.py
+++ tests/test_predict.py
@@ -21,17 +21,22 @@ batches = [load_data(subject=os.path.join(f'{path_to_data}',subject),
 
 def test_apply_model():
     m = train(ShuffledDataLoader([batches[0], batches[1]]), 
-              ShuffledDataLoader([batches[2]]), n_epochs=5, 
-              model_save_path=os.path.join(f'{path_to_data}', 'models', 'tmp_model.pth'))
+              ShuffledDataLoader([batches[2]]), n_epochs=5, spatial_model=False)
     predictions = apply_model([X for (X, y) in batches], m)
     for prediction, batch in zip(predictions, batches):
         assert prediction.shape == batch[1].shape
 
     
 def test_apply_pretrained_model():
+    model_save_path = os.path.join(f'{path_to_data}', 'models', 'tmp_model.pth')
+    m = train(ShuffledDataLoader([batches[0], batches[1]]), 
+              ShuffledDataLoader([batches[2]]), n_epochs=5, spatial_model=False,
+              model_save_path=model_save_path)
     predictions = apply_pretrained_model(
         [X for (X, y) in batches], 
-        os.path.join(f'{path_to_data}', 'models', 'tmp_model.pth')
+        os.path.join(f'{path_to_data}', 'models', 'tmp_model.pth'),
+        spatial_model=False
         )
     for prediction, batch in zip(predictions, batches):
         assert prediction.shape == batch[1].shape
+    os.remove(model_save_path)
