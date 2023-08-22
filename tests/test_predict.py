434af7a45990c26d30841799af45b78441b1ac54
Ying-Qiu Zheng <yingqiuz@users.noreply.github.com>
2023-08-22 16:29:43 +0100

    fix func name typo

--- tests/test_predict.py
+++ tests/test_predict.py
@@ -18,19 +18,20 @@ batches = [load_data(subject=os.path.join(f'{path_to_data}',subject),
                       target_path='streamlines/left', 
                       target_list=target_list, power=[1, 2], gamma=[0, 0.1]) for subject in subjects]
 
+
+def test_apply_model():
+    m = train(ShuffledDataLoader([batches[0], batches[1]]), 
+              ShuffledDataLoader([batches[2]]), n_epochs=5, 
+              model_save_path=os.path.join(f'{path_to_data}', 'models', 'tmp_model.pth'))
+    predictions = apply_model([X for (X, y) in batches], m)
+    for prediction, batch in zip(predictions, batches):
+        assert prediction.shape == batch[1].shape
+
     
 def test_apply_pretrained_model():
     predictions = apply_pretrained_model(
         [X for (X, y) in batches], 
         os.path.join(f'{path_to_data}', 'models', 'tmp_model.pth')
         )
-    for prediction, batch in zip(predictions, batches):
-        assert prediction.shape == batch[1].shape
-        
-def test_train():
-    m = train(ShuffledDataLoader([batches[0], batches[1]]), 
-              ShuffledDataLoader([batches[2]]), n_epochs=5, 
-              model_save_path=os.path.join(f'{path_to_data}', 'models', 'tmp_model.pth'))
-    predictions = apply_model([X for (X, y) in batches], m)
     for prediction, batch in zip(predictions, batches):
         assert prediction.shape == batch[1].shape
