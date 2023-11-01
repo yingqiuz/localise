828e561e0d74253b82b575c4a8bfd5df4fcb5816
Ying-Qiu Zheng <yingqiuz@users.noreply.github.com>
2023-11-01 16:48:49 +0000

    update argparse
    Former-commit-id: 08345264e516ea73118524d0676c2834e286224d

--- tests/test_scripts.py
+++ tests/test_scripts.py
@@ -9,34 +9,75 @@ def test_parse_arguments():
     with patch('argparse._sys.argv', 
                ['localise', '--train', 
                 '--subject', '/path/to/subject', 
-                '--mask', 'roi/mask',
-                '--label', 'roi/label',
+                '--seed', 'seed.nii.gz',
+                '--mask_dir', 'roi',
+                '--label', 'roi/label/label.nii.gz',
                 '--out_model', 'model',
-                '--target_path', 'data/left',
+                '--target_dir', 'tracts',
+                '--hemisphere', 'left',
+                '--data', 'data.npy',
                 '--epochs', '100']):
         args = parse_arguments()
 
         assert args.train
         assert args.subject == '/path/to/subject'
-        assert args.mask == 'roi/mask'
-        assert args.label == 'roi/label'
+        assert args.seed == 'seed.nii.gz'
+        assert args.mask_dir == 'roi'
+        assert args.label == 'roi/label/label.nii.gz'
         assert args.out_model == 'model'
+        assert args.target_dir == 'tracts'
+        assert args.data == 'data.npy'
         assert args.epochs == 100
 
     # Testing the prediction mode
     with patch('argparse._sys.argv', 
                ['localise', '--predict', 
                 '--subject', '/path/to/subject', 
-                '--mask', 'roi/mask',
-                '--data', 'data/data',
+                '--seed', 'seed.nii.gz',
+                '--mask_dir', 'roi',
+                '--target_dir', 'tracts',
+                '--data', 'data.npy',
+                '--hemisphere', 'right',
+                '--structure', 'vim',
                 '--out', 'output']):
         args = parse_arguments()
 
         assert args.predict
         assert args.subject == '/path/to/subject'
-        assert args.mask == 'roi/mask'
-        assert args.data == 'data/data'
+        assert args.mask_dir == 'roi'
+        assert args.data == 'data.npy'
         assert args.out == 'output'
+        assert args.seed == 'seed.nii.gz'
+        assert args.target_dir == 'tracts'
+        assert args.structure == 'vim'
+
+    # Testing the prediction mode with structure and model missing
+    with pytest.raises(SystemExit):
+        with patch('argparse._sys.argv', 
+                ['localise', '--predict', 
+                    '--subject', '/path/to/subject', 
+                    '--seed', 'seed.nii.gz',
+                    '--mask_dir', 'roi',
+                    '--target_dir', 'tracts',
+                    '--data', 'data.npy',
+                    '--hemisphere', 'right',
+                    '--out', 'output']):
+            args = parse_arguments()
+
+    # Testing the prediction mode with default atlas
+    with pytest.raises(SystemExit):
+        with patch('argparse._sys.argv', 
+                ['localise', '--predict', 
+                    '--subject', '/path/to/subject', 
+                    '--seed', 'seed.nii.gz',
+                    '--mask_dir', 'roi',
+                    '--target_dir', 'tracts',
+                    '--data', 'data.npy',
+                    '--hemisphere', 'right',
+                    '--model', 'model.pth',
+                    '--atlas', 'default',
+                    '--out', 'output']):
+            args = parse_arguments()
         
     # Test the missing argument in training mode
     with pytest.raises(SystemExit):
